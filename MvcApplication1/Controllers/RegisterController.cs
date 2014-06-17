5f64300f93ed09d184e029f1aa45eb4cfc205173
temporis <temporis@wa.home.pl>
2014-06-17 13:09:38 +0200

    Initial commit

---
+++ MvcApplication1/Controllers/RegisterController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication1.Models;
+using WebMatrix.WebData;
+using MvcApplication1.Filters;
+
+namespace MvcApplication1.Controllers
+{
+    [Authorize]
+    [InitializeSimpleMembership]
+    public class RegisterController : Controller
+    {
+       private Case model;
+
+        //
+        // GET: /Register/
+
+        public ActionResult Index()
+        {
+            MedicalCasesDataClassesDataContext mcDataContext = new MedicalCasesDataClassesDataContext();
+            var caseData = from c in mcDataContext.Cases
+                           select c;
+            return View(caseData);
+        }
+
+        //
+        // GET: /Register/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Register/Create
+
+         public ActionResult Create()
+        {
+            int userID = WebSecurity.CurrentUserId;
+            String currentUserName = WebSecurity.CurrentUserName;
+            MedicalCasesDataClassesDataContext mcDataContext = new MedicalCasesDataClassesDataContext();
+            var userData = from u in mcDataContext.Users
+                       where u.userName == currentUserName
+                       select u;
+            if (userData.Count().Equals(0)) {
+                //redirect /User/create
+                return RedirectToAction("Create", "User");
+            }
+            model = new Case();
+            model.userId = userID;
+            //walidator nie przpuszcza daty, chyba zły fomat, tworzenie poprzez wartość domyslną getdate()
+            model.Date = DateTime.Now;
+            ViewBag.userID = userID;
+            return View(model);
+        }
+
+        //
+        // POST: /Register/Create
+
+        [HttpPost]
+        public ActionResult Create(Case model)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                MedicalCasesDataClassesDataContext mcDCtx = new MedicalCasesDataClassesDataContext();
+                mcDCtx.Cases.InsertOnSubmit(model);
+                mcDCtx.SubmitChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Register/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Register/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Register/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            MedicalCasesDataClassesDataContext mcDataContext = new MedicalCasesDataClassesDataContext();
+            Case caseModel = mcDataContext.Cases.Single(u => u.Id == id);
+            mcDataContext.Cases.DeleteOnSubmit(caseModel);
+            mcDataContext.SubmitChanges();
+            return RedirectToAction("Index");
+        }
+
+        //
+        // POST: /Register/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MvcApplication1/Controllers/UserController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication1.Models;
+using WebMatrix.WebData;
+
+namespace MvcApplication1.Controllers
+{
+    public class UserController : Controller
+    {
+        //
+        // GET: /User/
+
+        public ActionResult Index()
+        {
+            MedicalCasesDataClassesDataContext mcDataContext = new MedicalCasesDataClassesDataContext();
+            var userData = from u in mcDataContext.Users
+                           select u;
+            return View(userData);
+        }
+
+        //
+        // GET: /User/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /User/Create
+
+        public ActionResult Create()
+        {
+            String currentUserName = WebSecurity.CurrentUserName;
+            User model = new User();
+            model.userName = currentUserName;
+            return View(model);
+        }
+
+        //
+        // POST: /User/Create
+
+        [HttpPost]
+        public ActionResult Create(User model)
+        //public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                //var username = model.userName;
+                MedicalCasesDataClassesDataContext mcDataCtx = new MedicalCasesDataClassesDataContext();
+                mcDataCtx.Users.InsertOnSubmit(model);
+                mcDataCtx.SubmitChanges();
+                return RedirectToAction("Create", "Register");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /User/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /User/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /User/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            MedicalCasesDataClassesDataContext mcDataContext = new MedicalCasesDataClassesDataContext();
+            User user = mcDataContext.Users.Single(u => u.Id == id);
+            mcDataContext.Users.DeleteOnSubmit(user);
+            mcDataContext.SubmitChanges();
+            return RedirectToAction("Index");
+        }
+
+        //
+        // POST: /User/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
